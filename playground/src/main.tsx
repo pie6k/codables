97a06b690271fae69226f21cf543cee22c50a249
Adam Pietrasiak <adam@pietrasiak.com>
2025-10-28 18:47:55 +0100

    finalizing initial version

--- playground/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import "./root.css";
-
-import App from "./App.tsx";
-import { createRoot } from "react-dom/client";
-
-const root = createRoot(document.getElementById("root")!);
-
-root.render(<App />);
